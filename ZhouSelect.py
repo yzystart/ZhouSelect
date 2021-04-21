49b5a141fe0e14f9e5c70895a35d5ea6e90857de
叶志远 <yezy@daishupei.com>
2021-04-21 10:41:10 +0800

    lazy select 2

--- ZhouSelect.py
+++ ZhouSelect.py
@@ -1,10 +1,10 @@
 import sys
-
 from selenium import webdriver
 from lxml import etree
 import os
 import requests
 import json
+
 
 config = {
     # #要操作的文件名，包括后缀名
@@ -15,39 +15,41 @@ config = {
     # "page":"1"
 }
 
+
+# 生成配置文件
+def init_config():
+    config = {
+        # 工作目录
+        'work_directory': '',
+        # 当前操作的excel
+        'excel': '',
+        # 上一次录入到的行
+        'last_line': ''
+    }
+    savefile()
+
 # 保存配置文件
 def savefile():
-    with open(os.path.abspath('.') + '/main.conf', encoding='utf-8', mode='w') as writefile:
+    with open(os.path.abspath('.') + '/config.json', encoding='utf-8', mode='w') as writefile:
         writefile.write(json.dumps(config, ensure_ascii=False))
 
-print('***************************************')
-print('**********欢迎来到老周查询器1.0***********')
-print('***************************************')
-print('*************选择一个选项？**************')
-print('**************1.单查询*****************')
-print('**************2.Excel录入**************')
-select=input()
-if select=='2':
-    input('八嘎！')
-    sys.exit()
-if select !='1':
-    input('八嘎！')
-    sys.exit()
-print('>>单查询')
-print('>当前的操作路径为', os.path.abspath('.'))
-# 驱动路径
-bro = webdriver.Chrome(executable_path=os.path.abspath('.') + '\\' + "chromedriver.exe")
-# 打开网页获取cookie
-bro.get("https://www.tianyancha.com/")
-#
-input('请在页面登录你的天眼查账号，登录完成后回到cmd窗口回车')
+# 读取配置文件
+def read_config():
+    try:
+        print('正在读取配置文件...')
+        # 读取配置文件
+        with open(os.path.abspath('.') + '/config.json', encoding='utf-8-sig', mode='r') as readfile:
+            # 读取文本
+            str_config = readfile.read()
+            # 转化为字典
+            config = json.loads(str_config)
+            print(config)
+    except FileNotFoundError:
+        print('没有配置文件,开始生成..')
+        init_config()
+        input('生成结束,请重新打开程序')
 
-Cookie=''
-# 设置cookie
-for cookie in bro.get_cookies():
-    Cookie=Cookie+cookie['name']+'='+cookie['value']+';'
-Cookie=Cookie[0:-1]
-print(Cookie)
+
 def query(org):
     query_url = "https://www.tianyancha.com/search?key="+org
     # 获取分公司headers
@@ -109,12 +111,41 @@ def query(org):
         print('没有查询到该公司',e)
 
 
+print('***************************************')
+print('**********欢迎来到老周查询器2.0***********')
+print('***************************************')
+print('*************选择一个选项？**************')
+print('***************************************')
+print('**************1.单查询*****************')
+print('**************2.Excel录入**************')
+print('***************************************')
+
+select=input()
+print('>当前的操作路径为', os.path.abspath('.'))
+if select=='2':
+    read_config()
+# 驱动路径
+bro = webdriver.Chrome(executable_path=os.path.abspath('.') + '\\' + "chromedriver.exe")
+# 打开网页获取cookie
+bro.get("https://www.tianyancha.com/")
+#
+input('请在页面登录你的天眼查账号，登录完成后回到cmd窗口回车')
+
+Cookie=''
+# 设置cookie
+for cookie in bro.get_cookies():
+    Cookie=Cookie+cookie['name']+'='+cookie['value']+';'
+Cookie=Cookie[0:-1]
+print(Cookie)
 if __name__ == "__main__":
 
-    while True:
-        try:
-            query(input('请输入一个机构：'))
-        except Exception as e:
-            print(e)
-            continue
+    if select=='1':
+        while True:
+            try:
+                query(input('请输入一个机构：'))
+            except Exception as e:
+                print(e)
+                continue
+    if select =='2':
+        read_config()
 
